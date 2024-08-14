bfbd33168f8148072d56fa9286e240baf52a9f5f
Muhammad Zulfikri <zulfikri@nawatech.co>
2024-08-14 16:52:19 +0700

    chore: Optimize image loading and update styles

--- src/app/(views)/page.js
+++ src/app/(views)/page.js
@@ -13,10 +13,14 @@ import adobeAfterEffects from '@/assets/images/adobe-after-effects.svg'
 import adobePremierePro from '@/assets/images/adobe-premiere-pro.svg'
 import adobeIndesign from '@/assets/images/adobe-indesign.svg'
 
+import DandZSpeaksAvatar from '@/assets/images/DandZSpeaks-avatar.png'
+import capcutLogo from '@/assets/images/capcut-logo.svg'
+import afterEffectsLogo from '@/assets/images/after-effects-logo.svg'
+
 const HomePage = () => {
   return (
     <>
-      <CContainer lg className="px-4">
+      <CContainer lg className="py-5">
         <div id="about-me" className="row gx-5">
           {/* Left */}
           <div className="col-12 col-md-6 align-self-end py-3">
@@ -79,6 +83,46 @@ const HomePage = () => {
           </div>
         </div>
       </CContainer>
+
+      {/* DandZSpeaks */}
+      <div id="dandz-speaks1" className="position-relative my-5 py-5">
+        <CContainer lg>
+          <div className="mx-auto w-100 text-center" style={{ maxWidth: '816px' }}>
+            <Image
+              alt="DandZSpeaks Avatar"
+              src={DandZSpeaksAvatar}
+              className="img-fluid rounded-circle"
+              width={80}
+              height={80}
+            />
+            <div className="display-3 fw-bold text-white mt-3 text-center">
+              I help DandZSpeaks Level Up Their Content.
+            </div>
+            <p className="text-white mt-4 mx-auto text-center" style={{ maxWidth: '537px' }}>
+              For a beginner creator, they generate decent views with their short content. This is
+              why <b>I can help you as a small creator to get decent amount of views</b> with great
+              editing!
+            </p>
+          </div>
+        </CContainer>
+
+        <div className="position-absolute top-50 start-0 translate-middle-y user-select-none">
+          <Image src={capcutLogo} />
+        </div>
+
+        <div className="position-absolute top-50 end-0 translate-middle-y user-select-none">
+          <Image src={afterEffectsLogo} />
+        </div>
+      </div>
+
+      <div id="dandz-speaks2" className="py-5">
+        <div className="ratio ratio-16x9">
+          <video autoPlay className="object-fit-cover" controls={false} loop playsInline muted>
+            <source src="/videos/DandZSpeaks-video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Link from 'next/link'
-import { CCol, CFooter, CSidebarBrand } from '@coreui/react-pro'
+import { CCol, CFooter, CRow, CSidebarBrand } from '@coreui/react-pro'
 import CIcon from '@coreui/icons-react'
 import {
   cibInstagram,
@@ -16,52 +16,62 @@ import { sygnet } from '@/public/brand/sygnet'
 
 const AppFooter = () => {
   return (
-    <CFooter className="row align-items-start py-5 text-white">
-      <CCol>
-        <CSidebarBrand href="/" className="me-3">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
-        </CSidebarBrand>
-        <b>Rell&apos;s Graphic</b>
-      </CCol>
-      <CCol>
-        <p className="fw-bold">Address</p>
-        <div className="d-flex">
-          <div>
-            <CIcon icon={cilLocationPin} className="d-inline-block me-2" />
-          </div>
+    <CFooter>
+      <CRow className="flex-grow-1 row row-cols-1 row-cols-lg-4 align-items-start py-5 mt-4 text-white">
+        {/* Brand */}
+        <CCol className="text-center text-lg-start py-4">
+          <CSidebarBrand href="/" className="me-3">
+            <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
+            <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+          </CSidebarBrand>
+          <b>Rell&apos;s Graphic</b>
+        </CCol>
+        {/* Address */}
+        <CCol className="text-center text-lg-start py-4">
+          <p className="d-none d-lg-block fw-bold">Address</p>
+          <div className="d-flex justify-content-center justify-content-lg-start w-100">
+            <div className="d-none d-lg-inline-block">
+              <CIcon icon={cilLocationPin} className="me-2" />
+            </div>
 
-          <small className="fw-normal">
-            Manunggal Street No.37, Kembangan, South Meruya, West Jakarta, Indonesia 11650
-          </small>
-        </div>
-      </CCol>
-      <CCol>
-        <p className="fw-bold">Let&apos;s Talk</p>
-        <div className="d-flex">
-          <Link
-            className="text-reset text-decoration-none"
-            href={'mailto:asyrofiefarrell@gmail.com'}
-          >
-            <small className="fw-normal">asyrofiefarrell@gmail.com</small>
-          </Link>
-        </div>
-        <div className="d-flex">
-          <div>
-            <CIcon icon={cilScreenSmartphone} className="d-inline-block me-2" />
+            <small className="fw-normal">
+              Manunggal Street No.37, Kembangan,
+              <br />
+              South Meruya, West Jakarta,
+              <br />
+              Indonesia 11650
+            </small>
           </div>
+        </CCol>
+        {/* Contact */}
+        <CCol className="text-center text-lg-start py-4">
+          <p className="d-none d-lg-block fw-bold">Let&apos;s Talk</p>
+          <div className="d-flex justify-content-center justify-content-lg-start w-100">
+            <Link
+              className="text-reset text-decoration-none"
+              href={'mailto:asyrofiefarrell@gmail.com'}
+            >
+              <small className="fw-normal">asyrofiefarrell@gmail.com</small>
+            </Link>
+          </div>
+          <div className="d-flex justify-content-center justify-content-lg-start ">
+            <div>
+              <CIcon icon={cilScreenSmartphone} className="d-inline-block me-2" />
+            </div>
 
-          <small className="fw-normal">+62 821 8232 4543</small>
-        </div>
-      </CCol>
-      <CCol>
-        <p className="fw-bold">Social Media</p>
-        <div className="d-flex">
-          <CIcon icon={cibInstagram} className="me-3" />
-          <CIcon icon={cibTiktok} className="me-3" />
-          <CIcon icon={cibTwitter} />
-        </div>
-      </CCol>
+            <small className="fw-normal">+62 821 8232 4543</small>
+          </div>
+        </CCol>
+        {/* Social Media */}
+        <CCol className="text-center text-lg-start py-4">
+          <p className="d-none d-lg-block fw-bold">Social Media</p>
+          <div className="d-flex justify-content-center justify-content-lg-start">
+            <CIcon icon={cibInstagram} className="me-3" />
+            <CIcon icon={cibTiktok} className="me-3" />
+            <CIcon icon={cibTwitter} />
+          </div>
+        </CCol>
+      </CRow>
     </CFooter>
   )
 }
